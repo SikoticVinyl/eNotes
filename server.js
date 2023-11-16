9f655428b93fd81ba93e739d6f199c7ee91c7416
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-15 17:40:08 -0700

    Updated Get and POSt for notes in server.js

--- server.js
+++ server.js
@@ -31,25 +31,23 @@ app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
 
-const notes = [];
-
 // GET request to fetch all notes
 app.get('/api/notes', (req, res) => {
-    // Read notes from db.json file
     fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'Error reading notes' });
         return;
       }
-      // Parse the JSON data from db.json
       const notes = JSON.parse(data);
-      res.json(notes); // Send the array of notes as JSON
+      res.json(notes);
     });
   });
-
+  
+  // POST request to add a new note
   app.post('/api/notes', (req, res) => {
-    // Read existing notes from db.json file
+    const newNote = req.body;
+  
     fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
       if (err) {
         console.error(err);
@@ -58,19 +56,15 @@ app.get('/api/notes', (req, res) => {
       }
   
       let notes = JSON.parse(data);
-      const newNote = req.body;
-      newNote.id = generateUniqueID(); // Function to generate a unique ID
+      notes.push(newNote);
   
-      notes.push(newNote); // Add the new note to the notes array
-  
-      // Write updated notes back to db.json file
       fs.writeFile(path.join(__dirname, 'db', 'db.json'), JSON.stringify(notes), (err) => {
         if (err) {
           console.error(err);
           res.status(500).json({ error: 'Error saving note' });
           return;
         }
-        res.json(notes); // Respond with the updated array of notes
+        res.json(newNote);
       });
     });
   });
