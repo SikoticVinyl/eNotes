470c9bcf5871f1812c7024d5cfc7ef379b6173e5
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-11-15 17:10:09 -0700

    added GET request to fetch notes from db.json

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3001;
@@ -20,3 +21,20 @@ app.get('/notes', (req, res) => {
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
+
+const notes = [];
+
+// GET request to fetch all notes
+app.get('/api/notes', (req, res) => {
+    // Read notes from db.json file
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Error reading notes' });
+        return;
+      }
+      // Parse the JSON data from db.json
+      const notes = JSON.parse(data);
+      res.json(notes); // Send the array of notes as JSON
+    });
+  });
